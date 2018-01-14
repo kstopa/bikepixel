14e481972ff320560330fda6480d7a04711e8ab2
kstopa <stopa.krzysztof.k@gmail.com>
2018-01-14 22:43:17 +0100

    Added basic documentation and first version of the code with one simple effect.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# bikepixel
+# Bike Pixel
+
 NeoMatrix with Arduino for a back pack light to ride safty by night
+
+## Components
+
+To build your own Bike Pixel you should have all the elements listed bellow:
+
+    * Arduino UNO (or any other Ardiuno board).
+    * 1 x NeoPixel 8x8 matrix
+    * 3 x touch switch
+    * 1 x 10 kΩ resistence
+    * 1 x 46 uf capacitator
+    * 1 x 6 AA battery holder (but you can try any other power source)
+    * 1 x on\off botton (optonal if your battery holder have one built-in)
+    * Cables and soldering stuff
+
+Optional (to include battery voltage meter):
+
+    TODO
+
+
+## Setup
+
+To compile the source you'll need some extra libraries:
+
+
---
+++ bikepixel_sketch/bikepixel_sketch.ino
@@ -0,0 +1,119 @@
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_NeoPixel.h>
+  #include <Adafruit_NeoMatrix.h>
+
+/*=========================================================================
+APPLICATION SETTINGS
+                          
+MATRIX DECLARATION        Parameter 1 = width of NeoPixel matrix
+                          Parameter 2 = height of matrix
+                          Parameter 3 = pin number (most are valid)
+                          Parameter 4 = matrix layout flags, add together as needed:
+NEO_MATRIX_TOP,
+NEO_MATRIX_BOTTOM,
+NEO_MATRIX_LEFT,
+NEO_MATRIX_RIGHT          Position of the FIRST LED in the matrix; pick two, e.g.
+                          NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+                          NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+                          rows or in vertical columns, respectively; pick one or the other.
+NEO_MATRIX_PROGRESSIVE,
+NEO_MATRIX_ZIGZAG         all rows/columns proceed in the same order, 
+                          or alternate lines reverse direction; pick one.
+
+                          See example below for these values in action.
+
+Parameter 5 = pixel type flags, add together as needed:
+                          
+NEO_KHZ800                800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+NEO_KHZ400                400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+NEO_GRB                   Pixels are wired for GRB bitstream (most NeoPixel products)
+NEO_RGB                   Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+-----------------------------------------------------------------------*/
+#define FACTORYRESET_ENABLE     1
+
+#define PIN                     6   // Which pin on the Arduino is connected to the NeoPixels?
+     
+// Example for NeoPixel 8x8 Matrix.  In this application we'd like to use it 
+// with the back text positioned along the bottom edge.
+// When held that way, the first pixel is at the top left, and
+// lines are arranged in columns, zigzag order.  The 8x8 matrix uses
+// 800 KHz (v2) pixels that expect GRB color data.
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+/*=========================================================================*/
+
+const int PIN_MODE_BTN = 2;
+const int PIN_ADD_BRG_BTN = 3;
+const int PIN_REM_BRG_BTN = 4;
+
+/** modes:
+ *  0 - off
+ *  1 - full red square
+ *  2 - red circle 
+ *  3 - red circle that changes in intensity
+ *  4 - changing red squares 
+ */
+int mode = 0;
+int buttonState = 0; 
+int brightState = 2;
+int brightStep = 2;
+int brightMax = 50;
+
+void setup(void)
+{
+  // Matrix initialization
+  matrix.begin();
+  matrix.setBrightness(brightState);
+ 
+  matrix.fillScreen(0);
+  matrix.show(); // This sends the updated pixel colors to the hardware.
+  // Other
+  pinMode(PIN_MODE_BTN, INPUT);
+}
+
+bool checkModeButton(int pin) {
+  buttonState = digitalRead(pin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if ((buttonState == HIGH) && (mode <= 9)) {
+    mode++;
+    return true;
+  } else if ((buttonState == HIGH) && (mode >= 10)){
+    // turn LED off:
+    mode = 0;
+    return true;
+  } else {
+    // No changes
+    return false;
+  }
+}
+
+void updateBrightEffect() {
+  brightState += brightStep;
+  if (brightState >= brightMax) {
+    brightState = brightMax;
+    brightStep = -2;
+  } else if (brightState <= 2) {
+    brightState = 2;
+    brightStep = 2;
+  }
+  matrix.setBrightness(brightState);
+  matrix.show();
+}
+    
+void loop() {
+  if (checkModeButton(PIN_MODE_BTN)) {
+    if (mode == 0) {
+      matrix.fillScreen(0);
+    } else if (mode == 1) {
+      matrix.fillScreen(matrix.Color(255, 0, 0));
+    }
+    matrix.show(); // Sends the updated pixel colors to the hardware.
+    delay(100);
+  }
+  if (mode == 1) {
+    updateBrightEffect();
+    delay(50);
+  }
+}
